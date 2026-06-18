dbc91bb89032a30851ce5e5d9185e3a860ae65c1
PipitoDev <joaovitorsguizzato@gmail.com>
2026-06-18 11:49:59 -0300

    Add SQL queries for activities 39 and 40

--- AtividadeMySQL_31-40.sql
+++ AtividadeMySQL_31-40.sql
@@ -42,3 +42,32 @@ FROM OrderDetails
 WHERE OrderID % 10 = 1
 ORDER BY Quantity, OrderID ASC
 LIMIT 8;
+
+-- Atividade 37
+SELECT OrderDetailID AS 'Ordenado por Detalhe do Pedido(ID)',
+	   OrderID AS 'ID do Pedido'
+FROM OrderDetails
+ORDER BY OrderDetailID, OrderID DESC
+LIMIT 4;
+
+-- Atividade 38
+SELECT ProductID AS 'Ordenado por ID do Produto',
+	   Quantity AS 'Quantidade'
+FROM OrderDetails
+WHERE Quantity > 30
+ORDER BY ProductID, Quantity ASC
+LIMIT 9;
+
+-- Atividade 39
+SELECT OrderID AS 'Ordenado por ID do Pedido',
+	   ProductID AS 'ID do Produto'
+FROM OrderDetails
+ORDER BY OrderID, ProductID ASC
+LIMIT 5;
+
+-- Atividade 40
+SELECT Quantity AS 'Ordenado por Quantidade',
+	   OrderDetailID AS 'Detalhe do Pedido(ID)'
+FROM OrderDetails
+ORDER BY Quantity, OrderDetailID DESC
+LIMIT 6;
